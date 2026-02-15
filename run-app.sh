9229f36ae0555c86444ee17c3ba5098beddfa026
ayobamiblackforce-100 <ayobamiblackforce@gmail.com>
2026-02-15 15:57:40 -0500

    Update run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -4,6 +4,6 @@ apt install python3-venv python3-pip
 python3 -m venv .venv
 source .venv/bin/activate
 pip install streamlit -y
-git clone repo
-cd repo-name
+git clone repo https://github.com/ayobamiblackforce-100/project-2.git
+cd project-2
 streamlit run app.py
