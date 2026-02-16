d0f83305aaccb3b501a8c6cde422af642fab8a5c
Ayobami Blackforce <ayobamiblackforce@gmail.com>
2026-02-16 11:50:27 -0500

    Update run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
 apt install python3-venv python3-pip -y
-python3 -m venv .venv
-source .venv/bin/activate
+python3 -m venv games-venv
+source games-venv/bin/activate
 pip install streamlit
 cd project-2
 streamlit run app.py
