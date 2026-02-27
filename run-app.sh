4dfb306c256fc62094be8232ae0f9c9816f670a1
Ayobami Blackforce <ayobamiblackforce@gmail.com>
2026-02-27 13:56:41 -0500

    Update run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -4,5 +4,5 @@ apt install python3-venv python3-pip -y
 python3 -m venv games-venv
 source games-venv/bin/activate
 pip install streamlit
-streamlit run guessing_game/app.py
-#streamlit run guessing_game/app.py --server.port 80
+streamlit run guessing-game/app.py
+#streamlit run guessing-game/app.py --server.port 80
